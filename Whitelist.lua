bf59339c8eda35bd4c85e1734112ea9a4de0ca48
Damcpros <104703290+Damcpros@users.noreply.github.com>
2023-06-22 22:07:01 -0400

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,5 +1,6 @@
 local whitelist = {
 	[1] = {},
-	[2] = {4692991274, 4728359599},
+	[2] = {},
+	[3] = {{id=4692991274,clr=Color3.fromRGB(115, 0, 255),tag="Damc"},{id=4728359599,clr=Color3.fromRGB(90, 0, 200),tag="Springs"}}
 }
 return whitelist
