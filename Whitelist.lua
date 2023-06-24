2aec3eac25b66a71c7536b71fcf9a2a9955870f6
FemboyLover <123281638+FemboyLover1@users.noreply.github.com>
2023-06-23 22:52:23 -0400

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,6 +1,6 @@
 local whitelist = {
 	[1] = {},
 	[2] = {},
-	[3] = {{id=4692991274,clr=Color3.fromRGB(115, 0, 255),tag="Damc"},{id=4737258383,clr=Color3.fromRGB(90, 0, 200),tag="Springs"}}
+	[3] = {{id=4692991274,clr=Color3.fromRGB(115, 0, 255),tag="Damc"},{id=4740178106,clr=Color3.fromRGB(90, 0, 200),tag="Springs"}}
 }
 return whitelist
