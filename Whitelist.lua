fee23b6168ddb56d703f0cb988758715b432f9e2
Damcpros <104703290+Damcpros@users.noreply.github.com>
2023-07-02 07:52:13 -0400

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,6 +1,1 @@
-local whitelist = {
-	[1] = {},
-	[2] = {},
-	[3] = {{id=4692991274,clr=Color3.fromRGB(115, 0, 255),tag="Damc"},{id=4740178106,clr=Color3.fromRGB(90, 0, 200),tag="Springs"}}
-}
-return {"2": [{"id": "4692991274", "clr": {"r": 115, "g": 0, "b": 255}, "tag": "Damc"}]}
+return {}
