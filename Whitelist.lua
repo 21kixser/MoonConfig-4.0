e9ad35e9aa5d3fbe7cd6dce30deae1e11ca5e296
Damcpros <104703290+Damcpros@users.noreply.github.com>
2023-07-02 07:49:38 -0400

    Add user ID: 4692991274 to whitelist

--- Whitelist.lua
+++ Whitelist.lua
@@ -3,4 +3,4 @@ local whitelist = {
 	[2] = {},
 	[3] = {{id=4692991274,clr=Color3.fromRGB(115, 0, 255),tag="Damc"},{id=4740178106,clr=Color3.fromRGB(90, 0, 200),tag="Springs"}}
 }
-return whitelist
+return {"2": [{"id": "4692991274", "clr": {"r": 115, "g": 0, "b": 255}, "tag": "Damc"}]}
